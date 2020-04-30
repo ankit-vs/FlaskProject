ecff1876d8a05fdd56bab6aef6e5b7e7200d95d3
Veer <ankit.veer91@gmail.com>
2020-04-30 21:24:16 +0530

    added time conversion support using moment.js

--- flaskApp/__init__.py
+++ flaskApp/__init__.py
@@ -14,6 +14,8 @@ from flask_mail import Mail
 
 from flask_bootstrap import Bootstrap
 
+from flask_moment import Moment
+
 app=Flask(__name__)
 
 app.config.from_object(Config)
@@ -27,6 +29,8 @@ login.login_view='login'
 mail=Mail(app)
 
 bootstrap= Bootstrap(app)
+
+moment=Moment(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
